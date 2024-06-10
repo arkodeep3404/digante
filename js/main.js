3dc2d1a6340520c5e887606724e81aa1477ede23
Arkodeep Chatterjee <arkodeep3404@gmail.com>
2024-06-10 14:14:39 +0530

    work going on

--- js/main.js
+++ js/main.js
@@ -266,24 +266,7 @@
 		$(".js-sticky-header").sticky({topSpacing:0});
 	};
 	siteSticky();
-
-	// navigation
-	var OnePageNavigation = function() {
-		var navToggler = $('.site-menu-toggle');
-		$("body").on("click", ".main-menu li a[href^='#'], .smoothscroll[href^='#'], .site-mobile-menu .site-nav-wrap li a", function(e) {
-			e.preventDefault();
-
-			var hash = this.hash;
-
-			$('html, body').animate({
-				'scrollTop': $(hash).offset().top
-			}, 600, 'easeInOutExpo', function(){
-				window.location.hash = hash;
-			});
-
-		});
-	};
-	OnePageNavigation();
+	
 
 	var siteScroll = function() {
 
